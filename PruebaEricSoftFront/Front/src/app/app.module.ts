bc3eb0e135461dcbac4e4dcdf89ab715377f5336
ncastillo <neifi03128@gmail.com>
2024-04-19 08:40:36 -0400

    Finalizacion

--- PruebaEricSoftFront/Front/src/app/app.module.ts
+++ PruebaEricSoftFront/Front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatPaginatorModule,
     HttpClientModule,
     MatIconModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule
   ],
   exports:[
     HttpClientModule
